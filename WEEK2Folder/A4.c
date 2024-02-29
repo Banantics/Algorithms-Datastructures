d233708702e74498204f8db78cd91d360359bf2c
Doufless1 <159914093+Doufless1@users.noreply.github.com>
2024-02-29 10:55:54 +0100

    Update A4.c

--- WEEK2Folder/A4.c
+++ WEEK2Folder/A4.c
@@ -1,21 +1,12 @@
-array_t* array_init(array_t *p_array, size_t capacity) { // version 2
- if (p_array != NULL) {
- p_array->data = malloc(sizeof(float[capacity]));
-p_array->count = 0;
- p_array->capacity = p_array->data != NULL ? capacity : 0;
- }
- return p_array;
- } /// This is the right one because this array its dynamic array and we have to release memory of the heap
+// program 2
+12 int main(void) {
+13 array_t *array = (array_t *) malloc(sizeof(array_t));
+14 array_init(array, 10);
+15 // .... do some work ...
+7
+16 // finally, clean up
+17 array_destroy(array);
+18 free(array);
+19 }
+/// Its the second one because this is the way we free memory of the heap 
 
-array_t* array_init(array_t *p_array, size_t capacity) { // version 1
- if (p_array != NULL){
- float data[capacity];
-
-
- p_array->capacity = capacity;
- p_array->data = data;
- p_array->count = 0;
- }
- return p_array;
- } /// this is wrong because we cant let the compilier free memory because out of the heap because this is how we free memory for the stack
-/// Also this is going to make the program have undefined behaviour.
